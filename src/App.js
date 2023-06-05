fb9fcf9165d3c52cfc12706aea95bab8bc963fa6
NiteshKG <niteshgupta8161@gmail.com>
2023-06-05 15:02:55 +0530

    first react commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import './App.css';
+
+function App() {
+const [count,setCount] = useState(0);
+const [showText,setShowText] = useState(true);
+
+  return (
+    <div>
+     <h1>{count}</h1>
+     <button onClick ={() =>{
+         setCount(count+1);
+         setShowText(!showText);
+     }} 
+     > click here </button>
+     {showText && <p>This denotes a even number</p>}
+    </div>
+  );
+};
+
+export default App;
